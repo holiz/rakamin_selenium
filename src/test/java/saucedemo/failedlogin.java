b237c8086e51ad56e05a36d705531935542db7a8
optimap <kholiskholis@gmail.com>
2023-10-30 21:52:32 +0700

    vilde  report

--- src/test/java/saucedemo/failedlogin.java
+++ src/test/java/saucedemo/failedlogin.java
@@ -1,20 +1,24 @@
 package saucedemo;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class login {
+import java.util.concurrent.TimeUnit;
+
+public class failedlogin {
     @Test
-    public void open_browser(){
+    public void open_browser() {
         WebDriver open;
         String basUrl = "https://www.saucedemo.com/";
         WebDriverManager.chromedriver().setup();
         open = new ChromeDriver();
         open.manage().window().maximize();
+        open.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         open.get(basUrl);
 
         WebElement username = open.findElement(By.id("user-name"));
@@ -24,13 +28,16 @@ public class login {
 
         WebElement password = open.findElement(By.id("password"));
         password.click();
-        password.sendKeys("secret_sauce");
+        password.sendKeys("secret_sauce1");
         password.getText();
 
         WebElement submit = open.findElement(By.id("login-button"));
         submit.isDisplayed();
         submit.click();
 
+        String errorlogin = submit.findElement(By.xpath("//*[@id=\"login_button_container\"]/div/form/div[3]/h3/button/svg")).getText();
+        Assert.assertEquals(errorlogin,"error salah password");
 
     }
+
 }
